8550940f47781b84c1d1fc4239edf9ac12b2a347
Dongximing <dongx34@myumanitoba.ca>
2021-09-09 16:34:59 -0500

    add required text

--- data_preprocess_mimic3.py
+++ data_preprocess_mimic3.py
@@ -51,14 +51,14 @@ def index_default(i, char):
 # note_events = dataframe_from_csv(os.path.join(args.mimic3_path, 'NOTEEVENTS.csv'))
 # note_event_text = note_events[['SUBJECT_ID', 'HADM_ID', 'TEXT']].to_csv(os.path.join(args.output_path, 'all_note_event.csv'), index=False)#SUBJECT_ID,HADM_ID,TEXT
 note_event_text = dataframe_from_csv(os.path.join(args.mimic3_path, 'all_note_event.csv'))
-diagnoses = read_icd_diagnoses_table(args.mimic3_path)
-procedures = read_icd_procedures_table(args.mimic3_path)
+# diagnoses = read_icd_diagnoses_table(args.mimic3_path)
+# procedures = read_icd_procedures_table(args.mimic3_path)
 # print("diagnoses",len(diagnoses))
 # print("procedures",len(procedures))
-diagnoses.to_csv(os.path.join(args.output_path, 'all_diagnoses.csv'), index=False)#SUBJECT_ID,HADM_ID,ICD9_CODE,LONG_TITLE
-procedures.to_csv(os.path.join(args.output_path, 'all_procedures.csv'), index=False)#SUBJECT_ID,HADM_ID,ICD9_CODE,LONG_TITLE
-procedures_and_diagnoses = combine_procedures_diagnoses(procedures,diagnoses)
-procedures_and_diagnoses.to_csv(os.path.join(args.output_path, 'all_diagnoses_with_procedures.csv'), index=False)
+# diagnoses.to_csv(os.path.join(args.output_path, 'all_diagnoses.csv'), index=False)#SUBJECT_ID,HADM_ID,ICD9_CODE,LONG_TITLE
+# procedures.to_csv(os.path.join(args.output_path, 'all_procedures.csv'), index=False)#SUBJECT_ID,HADM_ID,ICD9_CODE,LONG_TITLE
+# procedures_and_diagnoses = combine_procedures_diagnoses(procedures,diagnoses)
+# procedures_and_diagnoses.to_csv(os.path.join(args.output_path, 'all_diagnoses_with_procedures.csv'), index=False)
 # print("procedures_and_diagnoses",len(procedures_and_diagnoses))
 
 
