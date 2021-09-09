bb23d94486cbb745872d449b7054fe1ca5546109
Dongximing <dongx34@myumanitoba.ca>
2021-09-09 16:08:27 -0500

    procedures_and_diagnoses

--- data_preprocess_mimic3.py
+++ data_preprocess_mimic3.py
@@ -11,7 +11,13 @@ parser = argparse.ArgumentParser(description='Extract per-subject data from MIMI
 parser.add_argument('--mimic3_path', type=str,default="/home/dongxx/projects/def-mercer/dongxx/mimiciii/1.4")
 parser.add_argument('--output_path', type=str,default="/home/dongxx/projects/def-mercer/dongxx/mimiciii/1.4")
 args = parser.parse_args()
+def index_default(i, char):
 
+    try:
+        retval = i.index(char)
+    except ValueError:
+        retval = 100000
+    return retval
 
 def read_icd_diagnoses_table(mimic3_path):
     codes = dataframe_from_csv(os.path.join(mimic3_path, 'D_ICD_DIAGNOSES.csv'))
@@ -42,16 +48,46 @@ def combine_procedures_diagnoses(procedures,diagnoses):
     procedures_and_diagnoses = procedures.merge(diagnoses, how='inner', left_on=['SUBJECT_ID', 'HADM_ID'], right_on=['SUBJECT_ID', 'HADM_ID'])
     return procedures_and_diagnoses
 
-# note_events = dataframe_from_csv(os.path.join(args.mimic3_path, 'NOTEEVENTS.csv'))
-# note_event_text = note_events[['SUBJECT_ID', 'HADM_ID', 'TEXT']].to_csv(os.path.join(args.output_path, 'all_note_event.csv'), index=False)#SUBJECT_ID,HADM_ID,TEXT
+note_events = dataframe_from_csv(os.path.join(args.mimic3_path, 'NOTEEVENTS.csv'))
+note_event_text = note_events[['SUBJECT_ID', 'HADM_ID', 'TEXT']].to_csv(os.path.join(args.output_path, 'all_note_event.csv'), index=False)#SUBJECT_ID,HADM_ID,TEXT
 diagnoses = read_icd_diagnoses_table(args.mimic3_path)
 procedures = read_icd_procedures_table(args.mimic3_path)
-print("diagnoses",len(diagnoses))
-print("procedures",len(procedures))
-# diagnoses.to_csv(os.path.join(args.output_path, 'all_diagnoses.csv'), index=False)#SUBJECT_ID,HADM_ID,ICD9_CODE,LONG_TITLE
-# procedures.to_csv(os.path.join(args.output_path, 'all_procedures.csv'), index=False)#SUBJECT_ID,HADM_ID,ICD9_CODE,LONG_TITLE
+# print("diagnoses",len(diagnoses))
+# print("procedures",len(procedures))
+diagnoses.to_csv(os.path.join(args.output_path, 'all_diagnoses.csv'), index=False)#SUBJECT_ID,HADM_ID,ICD9_CODE,LONG_TITLE
+procedures.to_csv(os.path.join(args.output_path, 'all_procedures.csv'), index=False)#SUBJECT_ID,HADM_ID,ICD9_CODE,LONG_TITLE
 procedures_and_diagnoses = combine_procedures_diagnoses(procedures,diagnoses)
 procedures_and_diagnoses.to_csv(os.path.join(args.output_path, 'all_diagnoses_with_procedures.csv'), index=False)
-print("procedures_and_diagnoses",len(procedures_and_diagnoses))
+# print("procedures_and_diagnoses",len(procedures_and_diagnoses))
+
+
+texts = []
+def split_log_line(i):
+    """Splits a line at either a period or a colon, depending on which appears
+    first in the line.
+    """
+    if index_default(i, "chief complaint") < index_default(i, "history of present illness"):
+        a = i.split('chief complaint')[1]
+        b = 'Chief complaint' + a
+        texts.append(b)
+    elif index_default(i, "history of present illness") < 100000:
+        a = i.split('history of present illness')[1]
+        b = 'History of present illness' + a
+        texts.append(b)
+    else:
+        texts.append('')
+
+
+note_event_text['TEXT'] = note_event_text['TEXT'].str.lower()
+for text in note_event_text['TEXT']:
+    split_log_line(text)
+
+note_event_text['require_text'] = texts
+
+note_event_text = note_event_text.dropna(subset=['HADM_ID','require_text'])
+note_event_text = note_event_text[['SUBJECT_ID', 'HADM_ID','require_text']]
+
+note_event_require_text = note_event_text[['SUBJECT_ID', 'HADM_ID', 'require_text']].to_csv(os.path.join(args.output_path, 'all_require_text_event.csv'), index=False)#SUBJECT_ID,HADM_ID,TEXT
+
 
 
--- testing.py
+++ testing.py
@@ -1,7 +1,41 @@
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame({"Name": ["Alice", "Bob", "Mallory", "Mallory", "Bob", "Mallory"],
-                 "City":["Seattle", "Seattle", "Portland", "Seattle", "Seattle", "Portland"],
-                 "Val":[4, 3, 3, np.nan, np.nan, 4]})
-print(df)
+# df = pd.DataFrame({"Name": ["Alice", "Bob", "Mallory", "Mallory", "Bob", "Mallory"],
+#                  "City":["Seattle", "Seattle", "Portland", "Seattle", "Seattle", "Portland"],
+#                  "Val":[4, 3, 3, np.nan, np.nan, 4]})
+# print(df)
+#
+def index_default(i, char):
+    """Returns the index of a character in a line, or the length of the string
+    if the character does not appear.
+    """
+    try:
+        retval = i.index(char)
+    except ValueError:
+        retval = 100000
+    return retval
+
+s = []
+
+def split_log_line(i):
+    """Splits a line at either a period or a colon, depending on which appears
+    first in the line.
+    """
+    if index_default(i, "apple") < index_default(i, "banana"):
+        a = i.split('apple')[1]
+        # print(a)
+        b = 'apple' + a
+        s.append(b)
+
+    elif index_default(i, "banana") < 100000:
+        a = i.split('banana')[1]
+        print(a)
+        b = 'banana' + a
+        s.append(b)
+    else:
+        s.append('')
+text = ['apple are banana is','banana is apple are']
+for i in text:
+    split_log_line(i)
+print(s)
